a7ee8859a34fab324099f4496468b6e4a6507bf8
Kevin <lubbok0704@gmail.com>
2026-08-24 10:44:14 -0400

    chore: prepare v0.1.4-rc.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [Unreleased]
+## [0.1.4-rc.1] - 2026-08-24
 
 ### Added
 
--- tools/build-package.ps1
+++ tools/build-package.ps1
@@ -1,4 +1,4 @@
-param([string]$Version = '0.1.3')
+param([string]$Version = '0.1.4')
 
 $ErrorActionPreference = 'Stop'
 $root = Split-Path -Parent $PSScriptRoot
